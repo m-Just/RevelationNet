75320f83d9155fccefd1fbc8953ac5c37fbebba4
ryli <ryli>
2018-06-12 15:23:05 +0800

    testing different training scheme

--- hans_experiments/experiments.py
+++ hans_experiments/experiments.py
@@ -40,6 +40,10 @@ def data_cifar10(train_start, train_end, test_start, test_end):
     x_train /= 255
     x_test /= 255
 
+    x_train_mean = np.mean(x_train, axis=0)
+    x_train -= x_train_mean
+    x_test -= x_train_mean
+
     datagen = ImageDataGenerator(
         featurewise_center=False,  # set input mean to 0 over the dataset
         samplewise_center=False,  # set each sample mean to 0
@@ -69,8 +73,8 @@ def train_classifier(model_name, nb_epochs):
     datagen, (x_train, y_train), (x_test, y_test) = \
         data_cifar10(train_start, train_end, test_start, test_end)
 
-    label_smooth = .1
-    y_train = y_train.clip(label_smooth / 9., 1. - label_smooth)
+    #label_smooth = .1
+    #y_train = y_train.clip(label_smooth / 9., 1. - label_smooth)
 
     # Define input TF placeholder
     x = tf.placeholder(tf.float32, shape=(None, 32, 32, 3))
@@ -80,23 +84,54 @@ def train_classifier(model_name, nb_epochs):
     epoch_step = tf.Variable(0, trainable=False)
     if model_name == 'simple':
         model = make_simple_cnn()
-        learning_rate = 0.003
+        learning_rate = tf.constant(0.003)
     elif model_name == 'resnet':
         model = make_resnet(depth=32)
-        learning_rate = 0.001
-        learning_rate = tf.train.polynomial_decay(
+        learning_rate = tf.constant(0.001)
+        '''
+        learning_rate = tf.train.exponential_decay(
             learning_rate,
             global_step=epoch_step,
             decay_steps=nb_epochs, 
-            end_learning_rate=1e-6,
+            decay_rate=0.9441,
+            staircase=True)
+        learning_rate = tf.case([
+            (epoch_step > 180, lambda: 0.5e-6),
+            (epoch_step > 160, lambda: 1e-6),
+            (epoch_step > 120, lambda: 1e-5),
+            (epoch_step > 80, lambda: 1e-4)],
+            default=lambda: 1e-3)
+        learning_rate = tf.case([
+            (epoch_step > 70, lambda: 1e-5),
+            (epoch_step > 50, lambda: 1e-4)],
+            default=lambda: 1e-3)
+        lr1 = tf.train.polynomial_decay(
+            learning_rate,
+            global_step=epoch_step,
+            decay_steps=80,
+            end_learning_rate=1e-4,
             power=0.5)
+        lr1 = tf.train.exponential_decay(
+            learning_rate,
+            global_step=epoch_step,
+            decay_steps=80,
+            decay_rate=0.971628)
+        lr2 = tf.train.exponential_decay(
+            1e-4,
+            global_step=epoch_step-80,
+            decay_steps=120,
+            decay_rate=0.944061)
+        learning_rate = tf.cond(epoch_step < 80,
+                                true_fn=lambda: lr1,
+                                false_fn=lambda: lr2)
+        '''
     else:
         raise ValueError()
     #for layer in model.layers:
     #    print(layer.name)
     preds = model.get_probs(x)
 
-    batch_size = 128
+    batch_size = 32
     train_params = {
         'nb_epochs': nb_epochs,
         'batch_size': batch_size,
@@ -129,7 +164,7 @@ def train_classifier(model_name, nb_epochs):
     return report
 
 def main(argv=None):
-    train_classifier(model_name='resnet', nb_epochs=50)
+    train_classifier(model_name='resnet', nb_epochs=200)
 
 if __name__ == '__main__':
     tf.app.run()
