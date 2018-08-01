c2c6a7c00d7d2877fecffe1f5aa94c7257b13c31
ryli <ryli>
2018-08-02 01:19:51 +0800

    simplified code; add tracker to gradient_attack.py

--- experiments/backtracking.py
+++ experiments/backtracking.py
@@ -14,21 +14,22 @@ from skimage.transform import resize
 
 from classifiers import SimpleCNN, Resnet_v2_101
 import data_loader
-from gradient_attack import Generator
+from gradient_attack import Generator, Tracker
 
-def build_graph_init(Classifier, x, sess, expand_dim=False, pretrained=None, scope='conv'):
+def build_graph_init(Classifier, x, sess, expand_dim=False, pretrained=None,
+                     scope='conv'):
 
     model = Classifier(x, expand_dim=expand_dim)
     sess.run(tf.global_variables_initializer())
 
     if pretrained is not None:
-        var_list = [var for var in tf.global_variables() if var.op.name.startswith(scope)]
+        var_list = [var for var in tf.global_variables() if \
+                    var.op.name.startswith(scope)]
         saver = tf.train.Saver(var_list=var_list)
         saver.restore(sess, pretrained)
         
         print('Restored variables:')
-        for var in var_list:
-            print(var.op.name)
+        for var in var_list: print(var.op.name)
     
     return model
 
@@ -46,18 +47,6 @@ def eval_acc(logits, labels):
     correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
     return tf.reduce_mean(tf.cast(correct_pred, tf.float32), 0)
 
-def validate(sess, accuracy, x, y_, x_test, y_test):
-    acc_val = 0.
-    num_imgs = len(x_test)
-    num_batches = num_imgs / 100
-    for n_batch in range(num_batches):
-        x_batch = x_test[n_batch * 100 : (n_batch + 1) * 100]
-        y_batch = y_test[n_batch * 100 : (n_batch + 1) * 100]
-        batch_acc_val = sess.run(accuracy, feed_dict={x: x_batch, y_: y_batch})
-        acc_val += batch_acc_val
-    acc_val /= num_batches
-    print('Test accurcacy on legitimate images: %f' % acc_val)
-
 def evaluate_perturbation(p):
     abs_p = np.abs(p)
     l_one = np.sum(abs_p)
@@ -65,139 +54,58 @@ def evaluate_perturbation(p):
     l_inf = np.max(abs_p)
     return l_one, l_two, l_inf
     
-def adv_train():
-
-    # load training and testing data
-    (x_train, y_train), (x_test, y_test) = data_loader.load_original_data()
-    y_train = np.concatenate((y_train, np.zeros([len(y_train), 1], dtype=y_train.dtype)), axis=1)
-    y_test = np.concatenate((y_test, np.zeros([len(y_test), 1], dtype=y_test.dtype)), axis=1)
-    x_adv, y_adv = load_adversarial_samples()
-
-    for i in range(50000):
-        linf = np.max(np.abs(x_adv[i] - x_train[i]))
-        assert linf < 0.2 + 1e-6, linf
-
-    datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
-
-    # prepare model
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    sess = tf.Session(config=config)
-
-    x = tf.placeholder(tf.float32, [None, 32, 32, 3])
-    y_= tf.placeholder(tf.float32, [None, 11])
-
-    model = build_graph_init(x, sess, output_units=11, pretrained=None)
-
-    # define optimization process
-    loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=model.logits, labels=y_)
-    loss = tf.reduce_mean(loss)
-
-    tvars = tf.trainable_variables()
-    optimizer = tf.train.MomentumOptimizer(1e-2, 0.9, use_nesterov=True)
-    optim_step = optimizer.minimize(loss=loss, var_list=tvars)
-    sess.run(tf.variables_initializer(var_list=[var for var in tf.global_variables() if 'Momentum' in var.op.name]))
-    print('Training variables:')
-    for var in tvars:
-        print(var.op.name)
-
-    # define evaluation metrics
-    accuracy = eval_acc(model.logits, y_)
-
-    # start training
-    x_test_merge = np.concatenate([x_test, x_adv[49000:]], axis=0)
-    y_test_merge = np.concatenate([y_test, y_adv[49000:]], axis=0)
-    for n_epochs in range(50):
-        rand_ind = np.arange(49000, dtype=np.int32)
-        np.random.shuffle(rand_ind)
-        rand_ind = rand_ind[:5000]
-        x_train_merge = np.concatenate([x_train, np.take(x_adv, rand_ind, axis=0)], axis=0)
-        y_train_merge = np.concatenate([y_train, np.take(y_adv, rand_ind, axis=0)], axis=0)
-        datagen.fit(x_train_merge)
-        dataflow = datagen.flow(x_train_merge, y_train_merge, batch_size=128)
-
-        for n_iters in range(50000 / 128):
-            x_batch, y_batch = dataflow.next()
-            _, loss_val, acc_val = sess.run([optim_step, loss, accuracy], feed_dict={x: x_batch, y_: y_batch})
-            print('Epoch %d Step %d: loss=%f, accuracy=%f' % (n_epochs + 1, n_iters + 1, loss_val, acc_val))
-
-        loss_val = acc_val = 0.
-        for n_iters in range(11000 / 100):
-            x_batch = x_test_merge[n_iters*100 : (n_iters+1)*100]
-            y_batch = y_test_merge[n_iters*100 : (n_iters+1)*100]
-            batch_loss_val, batch_acc_val = sess.run([loss, accuracy], feed_dict={x: x_batch, y_: y_batch})
-            loss_val += batch_loss_val
-            acc_val += batch_acc_val
-        loss_val /= (11000 / 100)
-        acc_val /= (11000 / 100)
-        print('Epoch %d validation result: loss=%f, accuracy=%f' % (n_epochs + 1, loss_val, acc_val))
-
-    saver = tf.train.Saver(var_list=tvars)
-    saver.save(sess, '../saved_models/new_model/adv_model')
-
-def adv_test():
-    (x_train, y_train), (x_test, y_test) = data_loader.load_original_data()
-    x_adv, _ = load_adversarial_samples()
-    x_adv_test = x_adv[49000:]
-    y_adv_test = y_train[49000:]
-
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    sess = tf.Session(config=config)
-
-    x = tf.placeholder(tf.float32, [None, 32, 32, 3])
-    y_= tf.placeholder(tf.float32, [None, 10])
-
-    model = build_graph_init(x, sess, output_units=11, pretrained='../saved_models/new_model/adv_model')
-
-    accuracy = eval_acc(model.logits[:, :10], y_)
-    adv_detect_rate = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(model.logits, 1), 10), tf.float32), 0)
-
-    # test on legitimate images
-    validate(sess, accuracy, x, y_, x_test, y_test)
-
-    # test on adversarial images
-    acc_val = detect_rate_val = 0.
-    num_imgs = len(x_adv_test)
-    num_batches = num_imgs / 100
-    for n_batch in range(num_batches):
-        x_batch = x_adv_test[n_batch * 100 : (n_batch + 1) * 100]
-        y_batch = y_adv_test[n_batch * 100 : (n_batch + 1) * 100]
-        batch_detect_rate_val = sess.run(adv_detect_rate, feed_dict={x: x_batch})
-        batch_acc_val = sess.run(accuracy, feed_dict={x: x_batch, y_: y_batch})
-        detect_rate_val += batch_detect_rate_val 
-        acc_val += batch_acc_val
-    detect_rate_val /= num_batches
-    print('Detected rate on adversarial images: %f' % detect_rate_val)
-    acc_val /= num_batches
-    print('Test accurcacy on adversarial images: %f' % acc_val)
-
 def get_ranking(sess, logits):
     ranking = [(label, logit) for label, logit in enumerate(logits)]
     ranking.sort(key=lambda x: x[1], reverse=True)
     return [label for label, logit in ranking]
 
-def backtrack(attack_method='FG', target=None, eps_val=0.02, num_steps=100,
-              noise_scale=1., sigma=0.5, recover_num_steps=100,
+def direction_tracking(att_path, rec_path):
+
+    normalize = lambda x: x / np.sqrt(np.sum(x ** 2))
+
+    def stepwise(path, stepnum):
+        cumulative = normalize(path[-1] - path[0])
+        steps = [normalize(path[n] - path[n-1]) for n in range(1, stepnum)]
+        cossim = [np.sum(cumulative * step) for step in steps]
+        print('  Maximum=%f' % np.max(cossim))
+        print('  Minimum=%f' % np.min(cossim))
+        print('  Average=%f' % np.mean(cossim))
+        return cumulative, steps
+
+    att_stepnum = len(att_path)
+    rec_stepnum = len(rec_path)
+    print('Attack steps cosine similarity:')
+    att_cm, att_steps = stepwise(att_path, att_stepnum)
+    print('Recover steps cosine similarity:')
+    rec_cm, rec_steps = stepwise(rec_path, rec_stepnum)
+
+    print('Backtracking directional accuracy:')
+    print('  First step=%f' % np.sum(att_steps[-1] * (-rec_steps[0])))
+    print('  Last  step=%f' % np.sum(att_steps[0] * (-rec_steps[-1])))
+    print('  Cumulative=%f' % np.sum(att_cm * (-rec_cm)))
+
+def backtrack(attack_method='FG', target=None, eps_val=0.02, recover_eps_val=0.02,
+              num_steps=100, noise_scale=1., sigma=0.5, recover_num_steps=100,
               attack_optimize_method='sgd', recover_optimize_method='sgd',
-              recover_step_scale=1., recover_loss_thresh=None):
+              recover_step_scale=1., recover_loss_thresh=None, use_tracker=False):
 
     eps_val = eps_val * (IMAGE_MAX - IMAGE_MIN)
+    recover_eps_val = recover_eps_val * (IMAGE_MAX - IMAGE_MIN)
 
     # initiate attack
     targeted = (target is not None)
-    adversary1 = Generator(attack_method, IMGSIZE, IMAGE_MIN, IMAGE_MAX, x_adv_, model.logits, targeted=targeted, optimize_method=attack_optimize_method, cls_no=NUM_CLASSES)
-    adversary2 = Generator('FG', IMGSIZE, IMAGE_MIN, IMAGE_MAX, x_adv_, model.logits, targeted=False, optimize_method=recover_optimize_method, cls_no=NUM_CLASSES)
+    adversary1 = Generator(attack_method, IMGSIZE, IMAGE_MIN, IMAGE_MAX,
+                           x_adv_, model.logits, targeted=targeted,
+                           optimize_method=attack_optimize_method,
+                           cls_no=NUM_CLASSES)
+    if use_tracker:
+        adversary2 = Tracker(IMGSIZE, IMAGE_MIN, IMAGE_MAX, x_adv_, model.logits,
+                             num_classes=NUM_CLASSES)
+    else:
+        adversary2 = Generator('FG', IMGSIZE, IMAGE_MIN, IMAGE_MAX, x_adv_,
+                               model.logits, targeted=False,
+                               optimize_method=recover_optimize_method,
+                               cls_no=NUM_CLASSES)
 
     noise_sampling_n = 10
     avg_l1 = avg_l2 = avg_linf = 0.
@@ -224,8 +132,9 @@ def backtrack(attack_method='FG', target=None, eps_val=0.02, num_steps=100,
 
         # generate adversarial image from test set on fly
         print('Generating adversarial image %d' % i)
-        x_adv = adversary1.generate(sess, x_test[i], target_label,
-            eps_val=eps_val, num_steps=num_steps)[-1]
+        adv_path = adversary1.generate(sess, x_test[i], target_label,
+            eps_val=eps_val, num_steps=num_steps)
+        x_adv = adv_path[-1]
         y_adv = y_test[i]
         sess.run(assign_op, feed_dict={x: x_adv})
         logits, pred = sess.run([model.logits, prediction])
@@ -243,30 +152,33 @@ def backtrack(attack_method='FG', target=None, eps_val=0.02, num_steps=100,
         print('  Perturbation L2  : %f' % l2)
         print('  Perturbation Linf: %f' % linf)
 
-        # apply adequate noise to the adversarial image
-        print('Applying noise to adversarial image %d' % i)
-        max_loss = -1.
-        for n in range(noise_sampling_n): # search for random noise that maximize the loss
-            #noise = (np.random.rand(32, 32, 3) - 0.5) * eps_val * 2
-            noise = np.random.rand(IMGSIZE, IMGSIZE, 3)
-            noise = ((noise >= 0.5) - 0.5) * eps_val * 2 * noise_scale
-            noisy_img = x_adv + noise
-            sess.run(assign_op, feed_dict={x: noisy_img})
-            loss = sess.run(adversary1.loss, feed_dict={adversary1.y_adv: pred})
-            if loss > max_loss:
-                max_loss = loss
-                sample_x = noisy_img
-        sample_x = np.clip(sample_x, IMAGE_MIN, IMAGE_MAX)
         sample_y = y_adv
+        if noise_scale == 0.:
+            sample_x = x_adv
+            sess.run(assign_op, feed_dict={x: sample_x})
+        else: # apply adequate noise to the adversarial image
+            print('Applying noise to adversarial image %d' % i)
+            max_loss = -1.
+            for n in range(noise_sampling_n):
+                noise = np.random.rand(IMGSIZE, IMGSIZE, 3)
+                noise = ((noise >= 0.5) - 0.5) * eps_val * 2 * noise_scale
+                noisy_img = x_adv + noise
+                sess.run(assign_op, feed_dict={x: noisy_img})
+                loss = sess.run(adversary1.loss,
+                                feed_dict={adversary1.y_adv: pred})
+                if loss > max_loss:
+                    max_loss = loss
+                    sample_x = noisy_img
+            sample_x = np.clip(sample_x, IMAGE_MIN, IMAGE_MAX)
+            sess.run(assign_op, feed_dict={x: sample_x})
 
-        sess.run(assign_op, feed_dict={x: sample_x})
-        #if pred != sess.run(prediction): # noise should not change prediction
-        #    print('Noise changed prediction: skip image %d' % i)
-        #    continue
-        logits = sess.run(model.logits)
-        ranking = get_ranking(sess, logits[0])
-        gt_rank_noisy[ranking.index(lgt_pred)] += 1
-        print('  Class rankings (noisy adversarial):', ranking[:10])
+            #if pred != sess.run(prediction):
+            #    print('Noise changed prediction: skip image %d' % i)
+            #    continue
+            logits = sess.run(model.logits)
+            ranking = get_ranking(sess, logits[0])
+            gt_rank_noisy[ranking.index(lgt_pred)] += 1
+            print('  Class rankings (noisy adversarial):', ranking[:10])
 
         # attack the adversarial image
         print('Recovering adversarial image %d' % i)
@@ -274,12 +186,20 @@ def backtrack(attack_method='FG', target=None, eps_val=0.02, num_steps=100,
             # using noisy one as base is much better than the clean one
             clipping_base = sample_x
         else:
-            clipping_base = scipy.ndimage.filters.gaussian_filter(sample_x, sigma=sigma)
-        result_img = adversary2.generate(sess, clipping_base, pred,
-            eps_val=eps_val,
-            num_steps=int(recover_num_steps/recover_step_scale),
-            step_scale=recover_step_scale,
-            loss_thresh=recover_loss_thresh)
+            clipping_base = scipy.ndimage.filters.gaussian_filter(sample_x,
+                sigma=sigma)
+
+        if use_tracker:
+            result_path = adversary2.generate(sess, x_adv,
+                num_steps=int(recover_num_steps/recover_step_scale),
+                step_scale=recover_step_scale)
+        else:
+            result_path = adversary2.generate(sess, clipping_base, pred,
+                eps_val=recover_eps_val,
+                num_steps=int(recover_num_steps/recover_step_scale),
+                step_scale=recover_step_scale,
+                loss_thresh=recover_loss_thresh)
+        result_img = result_path[-1]
         result_imgs.append(result_img)
 
         # evaluate recovery status
@@ -304,6 +224,11 @@ def backtrack(attack_method='FG', target=None, eps_val=0.02, num_steps=100,
         avg_l2 += l2
         avg_linf += linf
 
+        adv_path = np.insert(adv_path, 0, x_test[i], axis=0)
+        result_path = np.insert(result_path, 0, sample_x, axis=0)
+        direction_tracking(adv_path, result_path)
+        #raw_input()
+
     recovery_rate = recovered / len(result_imgs)
     avg_l1 /= len(result_imgs)
     avg_l2 /= len(result_imgs)
@@ -316,9 +241,10 @@ def backtrack(attack_method='FG', target=None, eps_val=0.02, num_steps=100,
     print('Ranking percentage of ground truth label (clean adversarial)')
     for i in range(10):
         print('  #%d: %f' % (i + 1, gt_rank_adv[i] / len(result_imgs)))
-    print('Ranking percentage of ground truth label (noisy adversarial)')
-    for i in range(10):
-        print('  #%d: %f' % (i + 1, gt_rank_noisy[i] / len(result_imgs)))
+    if noise_scale > 0.:
+        print('Ranking percentage of ground truth label (noisy adversarial)')
+        for i in range(10):
+            print('  #%d: %f' % (i + 1, gt_rank_noisy[i] / len(result_imgs)))
     print('Ranking percentage of ground truth label (after attempted recovery)')
     for i in range(10):
         print('  #%d: %f' % (i + 1, gt_rank_rc[i] / len(result_imgs)))
@@ -417,25 +343,29 @@ if __name__ == '__main__':
     # targeted attack single step recovery
     #backtrack(target=1, noise_scale=0, sigma=None, recover_step_scale=100.)
 
+    '''
     backtrack(
         attack_method='FG',
-        target=None,
+        target=0,
         noise_scale=0.,
         sigma=None,
         attack_optimize_method='sgd',
         recover_optimize_method='sgd',
-        recover_step_scale=100.)
+        recover_step_scale=100.,
+        recover_eps_val=0.04,
+        use_tracker=False)
 
     '''
     parameter_dict = {
         'attack_method': ['FG'],
-        'target': [None],
-        'eps_val': [0.02, 0.08, 0.14, 0.20],
-        'noise_scale': [2.],# 0.25, 0.5, 1.0, 2.0, 4.0],
-        'sigma': [0.5],# 0.25, 0.5, 0.75, 1.0],
+        'target': [0],
+        'eps_val': [0.02],#[0.01, 0.02, 0.03, 0.04],# 0.08, 0.14, 0.20],
+        #'recover_eps_val': [0.01, 0.02, 0.04, 0.08],
+        'noise_scale': [0., 0.25, 0.5, 1.0, 2.0, 4.0],
+        'sigma': [None, 0.25, 0.5, 0.75, 1.0],
         'attack_optimize_method': ['sgd'],# 'momentum', 'adam'],
         'recover_optimize_method': ['sgd'],# 'momentum', 'adam'],
-        'recover_step_scale': [1., 10., 100.],
+        'recover_step_scale': [100.],# 10., 100.],
     }
     from itertools import product
     f = open('./logs/backtrack_results.txt', 'w')
@@ -452,4 +382,3 @@ if __name__ == '__main__':
         f.write('\n')
         f.flush()
     f.close()
-    '''
--- experiments/gradient_attack.py
+++ experiments/gradient_attack.py
@@ -1,22 +1,73 @@
+import scipy
 import tensorflow as tf
 
+class Tracker():
+    def __init__(self, imgsize, imgmin, imgmax, conv_input, logits,
+                 num_classes=10):
+        imgshape = (imgsize, imgsize, 3)
+        self.logits = logits
+        self.x = tf.placeholder(tf.float32, imgshape)
+        self.x_adv = conv_input
+        self.assign_op = tf.assign(self.x_adv, self.x)
+        self.y_ = tf.placeholder(tf.float32, [1, num_classes])
+        self.epsilon = tf.placeholder(tf.float32, ())
+        self.step_scale = tf.placeholder(tf.float32, ())
+        
+        self.loss = tf.nn.softmax_cross_entropy_with_logits_v2(
+            logits=logits, labels=self.y_)
+        self.optimizer = tf.train.GradientDescentOptimizer(1.)
+
+        gradient, var = self.optimizer.compute_gradients(
+            self.loss, var_list=[self.x_adv])[0]
+        step_size = self.step_scale * self.epsilon / tf.norm(gradient, ord=2)
+        grads_and_vars = [(step_size * gradient, var)]
+        self.optim_step = self.optimizer.apply_gradients(grads_and_vars)
+
+        below = self.x - self.epsilon
+        above = self.x + self.epsilon
+        projected = tf.clip_by_value(
+            tf.clip_by_value(self.x_adv, below, above), imgmin, imgmax)
+        with tf.control_dependencies([projected]):
+            self.project_step = tf.assign(self.x_adv, projected)
+
+    def generate(self, sess, image, eps_val=0.02, step_scale=1., num_steps=100):
+        result_imgs = []
+
+        blurred = scipy.ndimage.filters.gaussian_filter(image, sigma=0.01)
+        sess.run(self.assign_op, feed_dict={self.x: blurred})
+        guiding_label = sess.run(tf.nn.softmax(self.logits))
+
+        sess.run(self.assign_op, feed_dict={self.x: image})
+        for i in range(num_steps):
+            sess.run(self.optim_step, feed_dict={
+                self.y_: guiding_label,
+                self.epsilon: eps_val,
+                self.step_scale: step_scale})
+            sess.run(self.project_step, feed_dict={
+                self.x: image,
+                self.epsilon: eps_val})
+            result_imgs.append(sess.run(self.x_adv))
+
+        return result_imgs
+
 class Generator():
-    def __init__(self, method, imgsize, imgmin, imgmax, conv_input, logits, targeted=True, cls_no=10, optimize_method='sgd'):
+    def __init__(self, method, imgsize, imgmin, imgmax, conv_input, logits,
+                 targeted=True, cls_no=10, optimize_method='sgd'):
+
         imgshape = (imgsize, imgsize, 3)
         self.x = tf.placeholder(tf.float32, imgshape)
         self.y_adv = tf.placeholder(tf.int32, ())
         self.x_adv = conv_input
-        self.targeted = targeted
         self.epsilon = tf.placeholder(tf.float32, ())
         self.step_scale = tf.placeholder(tf.float32, ())
-        self.num_steps = tf.placeholder(tf.float32, ())
 
         self.assign_op = tf.assign(self.x_adv, self.x)
 
         labels = tf.one_hot(self.y_adv, cls_no)
+        self.loss = tf.nn.softmax_cross_entropy_with_logits_v2(
+            logits=logits, labels=[labels])
 
-        self.loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=[labels])
-        if self.targeted:
+        if targeted:
             min_objective = self.loss
         else:
             min_objective = -self.loss
@@ -36,41 +87,41 @@ class Generator():
             step_size = self.step_scale * self.epsilon / tf.norm(gradient, ord=2)
             grads_and_vars = [(step_size * gradient, var)]
             self.optim_step = optimizer.apply_gradients(grads_and_vars)
-            #self.optim_step = tf.train.GradientDescentOptimizer(step_size)\
-            #    .minimize(min_objective, var_list=[self.x_adv])
 
         elif method == 'FGS':
             gradient, _ = optimizer.compute_gradients(
                 -min_objective, var_list=[self.x_adv])[0]
-            l2_norm = (imgsize * imgsize * 3) ** 0.5
-            step_size = self.step_scale * self.epsilon / l2_norm
+            step_size = self.step_scale * self.epsilon / tf.norm(gradient, ord=2)
             pert = tf.sign(gradient) * step_size
             self.optim_step = tf.assign_add(self.x_adv, pert)
 
         below = self.x - self.epsilon
         above = self.x + self.epsilon
-        projected = tf.clip_by_value(tf.clip_by_value(self.x_adv, below, above), imgmin, imgmax)
+        projected = tf.clip_by_value(self.x_adv, below, above)
+        projected = tf.clip_by_value(projected, imgmin, imgmax)
         with tf.control_dependencies([projected]):
             self.project_step = tf.assign(self.x_adv, projected)
 
-    def generate(self, sess, image, target, eps_val=0.01, step_scale=1., num_steps=100, loss_thresh=None):
-        sess.run(tf.variables_initializer(self.optimizer.variables()))
+    def generate(self, sess, image, target, eps_val=0.01, step_scale=1.,
+                 num_steps=100, loss_thresh=None):
 
         adv_imgs = []
-        modified = image
+
+        sess.run(tf.variables_initializer(self.optimizer.variables()))
+        sess.run(self.assign_op, feed_dict={self.x: image})
+
         for i in range(num_steps):
-            sess.run(self.assign_op, feed_dict={self.x: modified})
             _, loss_val = sess.run([self.optim_step, self.loss],
-                feed_dict={self.y_adv: target,
-                           self.epsilon: eps_val,
-                           self.step_scale: step_scale,
-                           self.num_steps: num_steps})
+                                   feed_dict={self.y_adv: target,
+                                   self.epsilon: eps_val,
+                                   self.step_scale: step_scale})
                 
-            sess.run(self.project_step, feed_dict={self.x: image, self.epsilon: eps_val})
+            sess.run(self.project_step,
+                     feed_dict={self.x: image, self.epsilon: eps_val})
+
             if (i + 1) % 10 == 0:
                 print('  step %d, loss=%g' % (i+1, loss_val))
-            modified = sess.run(self.x_adv)
-            adv_imgs.append(modified)
+            adv_imgs.append(sess.run(self.x_adv))
 
             if loss_thresh is not None:
                 if loss_val > loss_thresh: break
